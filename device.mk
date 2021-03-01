e0d266e7d638c5b39b87b44ec433e66823cae5fe
Shoury Sharma <shanuu12e@gmail.com>
2021-03-01 17:56:12 +0000

    violet: Cleaned up

--- device.mk
+++ device.mk
@@ -19,7 +19,7 @@ $(call inherit-product, vendor/xiaomi/violet/violet-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe
+    $(LOCAL_PATH)/overlay-potato
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
